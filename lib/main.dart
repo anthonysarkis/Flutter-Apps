7811c1e3c50f715c73cbe17307e889eaf7ab3f0d
Anthony Sarkis <sarkis_anthony@hotmail.com>
2020-07-25 14:47:07 -0400

    ninja card

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,112 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  home: NinjaCard(),
+));
 
-class MyApp extends StatelessWidget{
+class NinjaCard extends StatefulWidget {
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("ap ba2"),
-          centerTitle: true,
-          backgroundColor: Colors.yellow[800],
-        ),
-        body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-          margin: EdgeInsets.all(30.0),
-          color: Colors.grey[400],
-          child: Text('Hello'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text("nikga"),
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+
+  int ninjaLevel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("Ninja ID Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],  
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/Artwork.png'),
+                radius: 70.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'BigoGotNokouilles',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              '$ninjaLevel',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  "patafrisse@hotmail.com",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
   }
 }
+
 
 // class MyApp extends StatelessWidget {
 //   // This widget is the root of your application.
