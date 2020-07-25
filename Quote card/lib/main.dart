b4d071be03dcf6733fc7f0896e9571f868b4c4a7
Anthony Sarkis <sarkis_anthony@hotmail.com>
2020-07-25 17:30:20 -0400

    quote card app

--- Quote card/lib/main.dart
+++ Quote card/lib/main.dart
@@ -1,111 +1,153 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quoteCard.dart';
 
-void main() => runApp(MaterialApp(
-  home: NinjaCard(),
-));
 
-class NinjaCard extends StatefulWidget {
+void main() => runApp(MaterialApp( home: QuoteList()));
+
+class QuoteList extends StatefulWidget {
   @override
-  _NinjaCardState createState() => _NinjaCardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
+class _QuoteListState extends State<QuoteList> {
 
-  int ninjaLevel = 0;
+  List<Quote> quotes = [
+    Quote(author: 'OS', text: 'Take it easssss'),
+    Quote(author: 'OS2', text: 'OS 4 LYFE'),
+    Quote(author: 'OS3', text: 'BigTime')
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text("Ninja ID Card"),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
+        title: Text('Awesome Quotes'),
+        backgroundColor: Colors.redAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            ninjaLevel += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],  
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget> [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/Artwork.png'),
-                radius: 70.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[800],
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0,),
-            Text(
-              'BigoGotNokouilles',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0,),
-            Text(
-              'CURRENT NINJA LEVEL',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0,),
-            Text(
-              '$ninjaLevel',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0,),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 10.0,),
-                Text(
-                  "patafrisse@hotmail.com",
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {
+            setState(() {
+              quotes.remove(quote);
+            });
+          }
+        )).toList(),
       ),
     );
   }
 }
+
+
+
+// void main() => runApp(MaterialApp(
+//   home: NinjaCard(),
+// ));
+
+// class NinjaCard extends StatefulWidget {
+//   @override
+//   _NinjaCardState createState() => _NinjaCardState();
+// }
+
+// class _NinjaCardState extends State<NinjaCard> {
+
+//   int ninjaLevel = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.grey[900],
+//       appBar: AppBar(
+//         title: Text("Ninja ID Card"),
+//         centerTitle: true,
+//         backgroundColor: Colors.grey[850],
+//         elevation: 0.0,
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           setState(() {
+//             ninjaLevel += 1;
+//           });
+//         },
+//         child: Icon(Icons.add),
+//         backgroundColor: Colors.grey[800],  
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: <Widget> [
+//             Center(
+//               child: CircleAvatar(
+//                 backgroundImage: AssetImage('assets/Artwork.png'),
+//                 radius: 70.0,
+//               ),
+//             ),
+//             Divider(
+//               height: 60.0,
+//               color: Colors.grey[800],
+//             ),
+//             Text(
+//               'NAME',
+//               style: TextStyle(
+//                 color: Colors.grey,
+//                 letterSpacing: 2.0,
+//               ),
+//             ),
+//             SizedBox(height: 10.0,),
+//             Text(
+//               'BigoGotNokouilles',
+//               style: TextStyle(
+//                 color: Colors.amberAccent[200],
+//                 letterSpacing: 2.0,
+//                 fontSize: 28.0,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             SizedBox(height: 30.0,),
+//             Text(
+//               'CURRENT NINJA LEVEL',
+//               style: TextStyle(
+//                 color: Colors.grey,
+//                 letterSpacing: 2.0,
+//               ),
+//             ),
+//             SizedBox(height: 10.0,),
+//             Text(
+//               '$ninjaLevel',
+//               style: TextStyle(
+//                 color: Colors.amberAccent[200],
+//                 letterSpacing: 2.0,
+//                 fontSize: 28.0,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             SizedBox(height: 30.0,),
+//             Row(
+//               children: <Widget>[
+//                 Icon(
+//                   Icons.email,
+//                   color: Colors.grey[400],
+//                 ),
+//                 SizedBox(width: 10.0,),
+//                 Text(
+//                   "patafrisse@hotmail.com",
+//                   style: TextStyle(
+//                     color: Colors.grey[400],
+//                     fontSize: 18.0,
+//                     letterSpacing: 1.0,
+//                   ),
+//                 )
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
 // class MyApp extends StatelessWidget {
